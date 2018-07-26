379d98bb44e0cabfc4ec48963d0b0e0ccb36b51c
lluis gifra <lluisgifra@hotmail.com>
2018-07-26 13:28:36 +0200

    iniciem projecta-1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    
+    <body class="details">
+    
+        <div class="links">
+        </div>
+        
+    </body>
+    
+</html>
